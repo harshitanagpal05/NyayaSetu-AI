644d307e0790b6f48ee40f8f6ec87ea86b471b68
ayush-kumar-24 <ayush2403kumar@gmail.com>
2026-04-17 00:12:45 +0530

    Implemented Supabase auth

--- app/services/supabase_client.py
+++ app/services/supabase_client.py
@@ -1,14 +1,23 @@
-import os
-from supabase import create_client, Client
 from dotenv import load_dotenv
+import os
 
-# ✅ load from ROOT folder (important)
-load_dotenv()
+# 👇 Get absolute root path
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+# 👇 Load .env from root
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+print("DEBUG PATH:", os.path.join(BASE_DIR, ".env"))
+print("DEBUG URL:", SUPABASE_URL)
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise Exception("Supabase environment variables not set")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+print("DEBUG URL:", SUPABASE_URL)
+print("DEBUG KEY:", SUPABASE_KEY)
